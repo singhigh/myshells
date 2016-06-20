c8549da25dd0a9e339570a7ce312c4c658c27a5c
singhigh <singhigh@users.noreply.github.com>
2016-06-20 18:53:00 +0800

    Create ovzssrins.sh

---
+++ ovzssrins.sh
@@ -0,0 +1,16 @@
+#wget -N --no-check-certificate https://raw.githubusercontent.com/singhigh/myshells/master/ovzssrins.sh && bash ovzssrins.sh
+curl https://debgen.simplylinux.ch/txt/jessie/sources_02afb983ca66b4136396fe1f3cc5e8052fa5532a.txt | sudo tee /etc/apt/sources.list
+apt-get update
+apt-get install python-pip m2crypto git vim -y
+pip install cymysql
+apt-get install build-essential
+wget https://github.com/jedisct1/libsodium/releases/download/1.0.10/libsodium-1.0.10.tar.gz
+tar xf libsodium-1.0.10.tar.gz && cd libsodium-1.0.10
+./configure && make -j2 && make install
+ldconfig
+git clone -b manyuser https://github.com/breakwa11/shadowsocks.git
+cd shadowsocks
+chmod 777 *.sh
+echo after setting the configuration, you may type bash run.sh to run the server.
+vim apiconfig.py
+cd ~/shadowsocks
