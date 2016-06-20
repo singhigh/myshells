8d5f60a155d00c28e9849d51d6cac48b9c88a513
singhigh <singhigh@users.noreply.github.com>
2016-06-20 19:05:18 +0800

    Update ovzssrins.sh

--- ovzssrins.sh
+++ ovzssrins.sh
@@ -1,5 +1,5 @@
 #wget -N --no-check-certificate https://raw.githubusercontent.com/singhigh/myshells/master/ovzssrins.sh && bash ovzssrins.sh
-curl https://debgen.simplylinux.ch/txt/jessie/sources_02afb983ca66b4136396fe1f3cc5e8052fa5532a.txt | sudo tee /etc/apt/sources.list
+#curl https://debgen.simplylinux.ch/txt/jessie/sources_02afb983ca66b4136396fe1f3cc5e8052fa5532a.txt | sudo tee /etc/apt/sources.list
 apt-get update
 apt-get install python-pip m2crypto git vim -y
 pip install cymysql
