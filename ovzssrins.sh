025d488761f01fa07332692452748d0819e030d6
singhigh <singhigh@users.noreply.github.com>
2016-06-27 14:04:52 +0800

    bug fixed

--- ovzssrins.sh
+++ ovzssrins.sh
@@ -3,14 +3,17 @@
 apt-get update
 apt-get install python-pip m2crypto git vim -y
 pip install cymysql
-apt-get install build-essential
+apt-get install build-essential -y
 wget https://github.com/jedisct1/libsodium/releases/download/1.0.10/libsodium-1.0.10.tar.gz
 tar xf libsodium-1.0.10.tar.gz && cd libsodium-1.0.10
 ./configure && make -j2 && make install
 ldconfig
+cd ~/
 git clone -b manyuser https://github.com/breakwa11/shadowsocks.git
-cd shadowsocks
+cd ~/shadowsocks
 chmod 777 *.sh
+cp config.json user-config.json
 echo after setting the configuration, you may type bash run.sh to run the server.
 vim apiconfig.py
+vim user-config.json
 cd ~/shadowsocks
