d5b2670a1d96fe15929a9908a37789aad6ca31db
singhigh <singhigh@users.noreply.github.com>
2016-08-14 22:52:43 +0800

    libsodium bug fix

--- centosvzssr.sh
+++ centosvzssr.sh
@@ -1,11 +1,13 @@
-#wget -N --no-check-certificate https://raw.githubusercontent.com/singhigh/myshells/master/ovzssrins.sh && bash ovzssrins.sh
+#wget -N --no-check-certificate https://raw.githubusercontent.com/singhigh/myshells/master/centosvzssr.sh && bash centosvzssr.sh
 yum update
 yum install wget python-pip m2crypto git vim -y
 pip install cymysql
 yum install build-essential -y
+yum -y groupinstall "Development Tools"
 wget https://github.com/jedisct1/libsodium/releases/download/1.0.10/libsodium-1.0.10.tar.gz
 tar xf libsodium-1.0.10.tar.gz && cd libsodium-1.0.10
 ./configure && make -j2 && make install
+echo /usr/local/lib > /etc/ld.so.conf.d/usr_local_lib.conf
 ldconfig
 cd ~/
 git clone -b manyuser https://github.com/breakwa11/shadowsocks.git
