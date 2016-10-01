5fd17e2722e125337d1e8077d7cf644446f10985
singhigh <singhigh@users.noreply.github.com>
2016-10-01 22:18:12 +0800

    Update aes256vzsimple.sh

--- aes256vzsimple.sh
+++ aes256vzsimple.sh
@@ -1,7 +1,8 @@
 apt-get update
-apt-get install python-pip m2crypto git vim curl -y && pip install cymysql
+apt-get install python-pip m2crypto git vim curl unzip -y && pip install cymysql
 cd ~/
-git clone -b manyuser https://github.com/breakwa11/shadowsocks.git
+wget https://github.com/breakwa11/shadowsocks/archive/52041387f9ee9961e6e5fdb0099c48c8d6a9aa94.zip && unzip 52041387f9ee9961e6e5fdb0099c48c8d6a9aa94.zip
+mv shadowsocks-52041387f9ee9961e6e5fdb0099c48c8d6a9aa94 shadowsocks
 cd ~/shadowsocks
 chmod 777 *.sh
 cp config.json user-config.json
